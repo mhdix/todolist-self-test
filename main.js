ce344c9e286d03abb1fa000b23322881edf9bce6
mhdix <domanloom@gmail.com>
2024-07-17 03:13:56 -0700

    create add todo logic

--- main.js
+++ main.js
@@ -1,81 +1,44 @@
 let todos = [];
-
 // select
-const todoInput = document.querySelector(".todo-input");
 const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".todo-input");
 const todoList = document.querySelector(".todolist");
-
-const todoFilter = document.querySelector(".filter-todos");
 
 // event
 todoForm.addEventListener("submit", addNewTodo);
-todoFilter.addEventListener("change", filterTodo);
-// function
 
+//  function
+
+// todo add todo
 function addNewTodo(e) {
   e.preventDefault();
 
-  if (!todoInput.value) return null
-
   const newTodo = {
     id: Date.now(),
-    createdAt: new Date().toISOString(),
     title: todoInput.value,
-    completed: false,
+    createAt: new Date().toISOString(),
+    isCompleted: false,
   };
 
   todos.push(newTodo);
   createTodo(todos);
+  todoInput.value = "";
 }
-
+// todo create
 function createTodo(todos) {
   let result = "";
+
 
   todos.forEach((todo) => {
     result += `
         <li class="todo">
-          <p class="todo__title">${todo.title}</p>
-          <span class="todo__createdAt">1402/1/28</span>
-          <button data-todo-id=${todo.id} class="todo__check"><i class="far fa-check-square"></i></button>
-          <button data-todo-id=${todo.id} class="todo__remove"><i class="far fa-trash-alt"></i></button>
+            <p class="todo__title">${todo.title}</p>
+            <span class="todo__createdAt">${new Date(todo.createAt).toLocaleDateString('fa-IR')}</span>
+            <button><i class="todo__check far fa-check-square"></i></button>
+            <button><i class="todo__remove far fa-trash-alt"></i></button>
         </li>
-        `;
+      `;
   });
+
   todoList.innerHTML = result;
-  todoInput.value = "";
-
-  // todo delete
-  const todoDeleteBtn = [...document.querySelectorAll(".todo__remove")];
-  todoDeleteBtn.forEach((btn) => btn.addEventListener("click", removeTodos));
-}
-
-function removeTodos(e) {
-  const todoId = Number(e.target.dataset.todoId);
-  todos = todos.filter((todo) => todo.id !== todoId);
-  createTodo(todos);
-}
-
-// todo filter
-
-function filterTodo(e) {
-  const filter = e.target.value;
-
-  switch (filter) {
-    case "all": {
-      createTodo(todos);
-      break;
-    }
-    case "completed": {
-      const filter = todos.filter((todo) => todo.completed);
-      createTodo(filter);
-      break;
-    }
-    case "uncomplated": {
-      const filter = todos.filter((todo) => !todo.completed);
-      createTodo(filter);
-      break;
-    }
-    default:
-      createTodo(todos);
-  }
 }
