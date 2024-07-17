a2b970a17d8f07d450ba90fdd78de323c62c4d23
mhdix <domanloom@gmail.com>
2024-07-17 04:03:37 -0700

    create delete todo logic

--- main.js
+++ main.js
@@ -28,17 +28,33 @@ function addNewTodo(e) {
 function createTodo(todos) {
   let result = "";
 
-
   todos.forEach((todo) => {
     result += `
         <li class="todo">
             <p class="todo__title">${todo.title}</p>
-            <span class="todo__createdAt">${new Date(todo.createAt).toLocaleDateString('fa-IR')}</span>
+            <span class="todo__createdAt">${new Date(
+              todo.createAt
+            ).toLocaleDateString("fa-IR")}</span>
             <button><i class="todo__check far fa-check-square"></i></button>
-            <button><i class="todo__remove far fa-trash-alt"></i></button>
+            <button class="todo__remove" data-todo-id=${
+              todo.id
+            }><i class=" far fa-trash-alt"></i></button>
         </li>
       `;
   });
 
   todoList.innerHTML = result;
+
+  //! delete todo
+  const deleteBtn = [...document.querySelectorAll(".todo__remove")];
+  deleteBtn.forEach((btn) => btn.addEventListener("click", deleteTodo));
+}
+
+// todo Delete
+function deleteTodo(e) {
+  const todoId = Number(e.target.dataset.todoId);
+  todos = todos.filter((todo) => todo.id !== todoId);
+
+  createTodo(todos);
+
 }
